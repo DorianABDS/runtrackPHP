053ec0d41972e77f9f0dc6da5d924c7cd4d2ae31
Dorian ABDS <dorian.abbadessa@laplateforme.io>
2025-01-15 15:23:36 +0100

    test: afficher un tableau avec les nombres pair et impair

---
+++ day3/job1/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$array = array(200, 204, 173, 98, 171, 404, 459);
+
+foreach ($array as $value) {
+    if ($value % 2 == 1) {
+        echo "$value est impair<br>";
+    } else {
+        echo "$value est pair<br>";
+    }
+}
