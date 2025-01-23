3f9a648c6218d9de4173c60d78d238b22db714b0
Dorian ABDS <dorian.abbadessa@laplateforme.io>
2025-01-23 15:49:19 +0100

    test: À l’aide d’une requête SQL, sélectionnez dans une colonne nommée 'capacite_totale' la somme des capacités des salles. Affichez le résultat de cette requête dans un tableau HTML

---
+++ day9/job8/index.php
@@ -0,0 +1,41 @@
+<?php
+
+$user = 'root';
+$pass = '';
+
+$db = new mysqli('localhost', $user, $pass, 'jour08');
+
+$request = "SELECT SUM(capacite) as capacite FROM salles";
+
+$query = $db->query($request);
+
+echo "<table border='1'>
+            <thead>
+                <tr>
+                    <th>capacite_totale</th>
+                </tr>
+            </thead>
+            <tbody>";
+while ($row = $query->fetch_array()) {
+    echo "<tr>
+            <td>" . $row['capacite'] . "</td>
+          </tr>";
+}
+echo "</tbody>
+      </table>";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
